bd8f257c3427a07f40984f4ea0fae1eb0b4ca509
Jimmy Le <jimmyle414@gmail.com>
2020-03-25 06:34:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,6 +23,11 @@ end
 #   array[1], array[2] = array[2], array[1]
 #   array
 # end
+
+def swap_elements_from_to(array, index, new_index)
+  array(index), array(new_index) = array(new_index), array(index)
+  return array
+end
   
 def reverse_array(array)
   array.reverse
