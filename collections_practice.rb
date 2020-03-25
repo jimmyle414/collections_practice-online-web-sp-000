e3d604206926551122af302d2cab9a223a0a2a59
Jimmy Le <jimmyle414@gmail.com>
2020-03-25 06:36:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,7 +25,7 @@ end
 # end
 
 def swap_elements_from_to(array, index, new_index)
-  array[index], array[new_index] = array[new_index), array(index)
+  array[index], array[new_index] = array[new_index], array[index]
   return array
 end
   
